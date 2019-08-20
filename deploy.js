8c6c8f99b6b949e3997fde496833b5031760487c
Rohit R. Abbadi <rohit@infito.com>
2019-08-20 12:34:30 +0530

    quite unzipping and pip install

--- deploy.js
+++ deploy.js
@@ -11,21 +11,24 @@ function sEx(cmd){
 }
 
 function unzipBuild(file, out_folder){
-    sEx(`unzip -o ${file} -d ${out_folder} -q`);
+    sEx(`unzip -o -q ${file} -d ${out_folder}`);
+    console.log("unzip complete...");
 }
 
 function setupVenv(venv_folder){
     sEx(`python3 -m venv ${venv_folder}`);
+    console.log("venv setup complete...");
 }
 
 function installDependencies(venv_folder, current_folder){
     sEx(`which python`);
     sEx(`python --version`);
     // let sourceIt = `source ${venv_folder}/bin/activate`;
-    let cdToCurrent = `cd ${current_folder}`;
+    // let cdToCurrent = `cd ${current_folder}`;
     sEx(`sudo chown -R ${os.userInfo().username} ${config.base_folder}`);
     sEx(`pip install -q WARNING wheel`);
     sEx(`pip install -q WARNING -r ${current_folder}/requirements.txt`);
+    console.log("requirements installed...");
     // sEx(`${sourceIt} && ${cdToCurrent} && python manage.py collectstatic --noinput`);
 }
 
